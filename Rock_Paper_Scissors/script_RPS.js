d6dfbacabdca735c489994a74b69deb2a3701e25
Geneva Diaz <kukla1422@gmail.com>
2021-09-25 08:46:17 -0500

    removed files that were inadvertantly added from last push

--- Rock_Paper_Scissors/script_RPS.js
+++
@@ -1,8 +0,0 @@
-var plyrContinue = confirm("Pres ok to get true response value. Press cancel to get false response value");
-var userChoice = prompt("Enter your choice 'R', 'P', or 'S':").toUpperCase();
-
-var validChoices = ["R", "P", "S"];
-
-if (!validChoices.includes(userChoice)) {
-  alert("Invalid choice");
-}
